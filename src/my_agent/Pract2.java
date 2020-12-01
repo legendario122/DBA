ba6c1e6d55c6b6b443d8e504c04461e7a40e3ac8
Adrian <adriangs122@gmail.com>
2020-12-01 12:32:46 +0100

    Ahora si

--- src/my_agent/Pract2.java
+++ src/my_agent/Pract2.java
@@ -8,12 +8,14 @@ package my_agent;
 import AppBoot.ConsoleBoot;
 
 public class Pract2{
-
+    
     public static void main(String[] args) {
+        /**
         ConsoleBoot _app = new ConsoleBoot("Pract2",args);
         _app.selectConnection();
         _app.launchAgent("BB99VA", MyWorldExplorer.class);
         _app.shutDown();
+        * **/
     }
 
     
